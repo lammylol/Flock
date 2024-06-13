6cc205f75c0a2e27b4a0943227b9f071dbfc0d92
lammylol <44993071+lammylol@users.noreply.github.com>
2024-06-12 23:36:31 -0700

    Adjustments to Empty Feed Label

--- Prayer Calendar/Views/Prayer Feed View/FeedRequestsRowView.swift
+++ Prayer Calendar/Views/Prayer Feed View/FeedRequestsRowView.swift
@@ -81,29 +81,57 @@ struct FeedRequestsRowView: View {
 //                    }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         .overlay {
-            // Only show this if this account is saved under your userID.
-            if person.username == "" || person.userID == userHolder.person.userID {
                 if viewModel.prayerRequests.isEmpty && viewModel.isFinished {
                     VStack{
                         ContentUnavailableView {
                             Label("No Posts Available...Yet!", systemImage: "list.bullet.rectangle.portrait")
                         } description: {
+                            if profileOrFeed == "feed" {
                                 if viewModel.selectedStatus == .pinned {
                                     Text("You will need to pin an exisiting post to see posts here")
                                 } else if viewModel.selectedStatus == .answered {
-                                    Text("None of your posts have been 'answered' yet, but that's okay! They will be featured here once you change the status to answered.")
+                                    Text("No posts have been 'answered' yet. They will be featured here once you change the status to answered.")
+                                } else if viewModel.selectedStatus == .noLongerNeeded {
+                                    Text("No posts have been marked as 'no longer needed.'")
+                                } else {
+                                    Text("Link friends to your calendar to start seeing prayer requests. Or, you can add prayers to your profile or to a private account and they'll also be featured here.")
+                                }
+                            } else {
+                                if viewModel.selectedStatus == .pinned {
+                                    Text("You will need to pin an exisiting post to see posts here")
+                                } else if viewModel.selectedStatus == .answered {
+                                    // Only show this if this account is saved under your userID.
+                                    if person.username == "" || person.userID == userHolder.person.userID {
+                                        Text("None of your posts have been 'answered' yet, but that's okay! They will be featured here once you change the status to answered.")
+                                    } else {
+                                        Text("\(person.firstName) has no answered posts yet.")
+                                    }
+                                } else if viewModel.selectedStatus == .answered {
+                                    Text("") // Empty. Just shows the main description.
                                 } else {
                                     if person.username == "" || person.userID == userHolder.person.userID {
-                                        Text("Start adding prayer requests to your list")
+                                        Text("Start adding posts to the list")
                                     } else {
-                                        Text("Link friends to your calendar to start seeing prayer requests. Or, you can add prayers to your profile or to a private account and they'll also be featured here.")
+                                        Text("\(person.firstName) has no posts to share.")
                                     }
                                 }
+                            }
                         } actions: {
-                            if person.username == "" || person.userID == userHolder.person.userID {
-                                Button(action: {showSubmit.toggle() })
-                                {
-                                    Text("Add Prayer Request")
+                            if profileOrFeed == "feed" {
+                                if viewModel.selectedStatus == .current {
+                                    Button(action: {showSubmit.toggle() })
+                                    {
+                                        Text("Add a Post")
+                                    }
+                                }
+                            } else {
+                                if viewModel.selectedStatus == .current {
+                                    if person.username == "" || person.userID == userHolder.person.userID {
+                                        Button(action: {showSubmit.toggle() })
+                                        {
+                                            Text("Add a Post")
+                                        }
+                                    }
                                 }
                             }
                         }
@@ -111,7 +139,6 @@ struct FeedRequestsRowView: View {
                         .offset(y: 120)
                         Spacer()
                     }
-                }
             }
         }
         .sheet(isPresented: $showSubmit, onDismiss: {
