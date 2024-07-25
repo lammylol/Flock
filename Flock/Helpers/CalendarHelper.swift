27568b04ef4e86294e5050d168212bca39931b81
pmar116 <preston.mar@live.com>
2024-07-24 22:07:23 -0700

    Update CalendarHelper.swift

--- Flock/Helpers/CalendarHelper.swift
+++ Flock/Helpers/CalendarHelper.swift
@@ -5,74 +5,87 @@
 //  Created by Matt Lam on 9/26/23.
 //
 
-import Foundation
-import FirebaseFirestore
+//
+//  CalendarHelper.swift
+//  PrayerCalendar
+//
+//  Created by Matt Lam on 9/26/23.
+//
 
-class CalendarHelper
-{
-    let calendar = Calendar.current
+import Foundation
+
+class CalendarHelper {
+
+    private let calendar = Calendar.current
+    private let dateFormatter = DateFormatter()
     
-    func plusMonth(date: Date) -> Date
-    {
-        return calendar.date(byAdding: .month, value:1, to: date)!
+    func plusMonth(to date: Date) -> Date {
+        guard let newDate = calendar.date(byAdding: .month, value: 1, to: date) else {
+            fatalError("Could not add month to date")
+        }
+        return newDate
     }
     
-    func minusMonth(date: Date) -> Date
-    {
-        return calendar.date(byAdding: .month, value:-1, to: date)!
+    func minusMonth(from date: Date) -> Date {
+        guard let newDate = calendar.date(byAdding: .month, value: -1, to: date) else {
+            fatalError("Could not subtract month from date")
+        }
+        return newDate
     }
     
-    func addDay(date: Date, count: Int) -> Date
-    {
-        return calendar.date(byAdding: .day, value:count, to: date)!
+    func addDays(to date: Date, count: Int) -> Date {
+        guard let newDate = calendar.date(byAdding: .day, value: count, to: date) else {
+            fatalError("Could not add days to date")
+        }
+        return newDate
     }
     
-    func monthString(date: Date) -> String
-    {
-        let dateFormatter = DateFormatter()
+    func monthString(from date: Date) -> String {
         dateFormatter.dateFormat = "LLLL"
         return dateFormatter.string(from: date)
     }
     
-    func yearString(date: Date) -> String
-    {
-        let dateFormatter = DateFormatter()
+    func yearString(from date: Date) -> String {
         dateFormatter.dateFormat = "yyyy"
         return dateFormatter.string(from: date)
     }
     
-    func daysInMonth(date: Date) -> Int
-    {
-        let range = calendar.range(of: .day, in: .month, for: date)!
+    func daysInMonth(for date: Date) -> Int {
+        guard let range = calendar.range(of: .day, in: .month, for: date) else {
+            fatalError("Could not get days in month")
+        }
         return range.count
     }
     
-    func dayOfMonth(date: Date) -> Int
-    {
-        let components = calendar.dateComponents([.day], from: date)
-        return components.day!
+    func dayOfMonth(for date: Date) -> Int {
+        guard let day = calendar.dateComponents([.day], from: date).day else {
+            fatalError("Could not get day of month")
+        }
+        return day
     }
     
-    func firstDayOfMonth(date: Date) -> Date
-    {
-        let components = calendar.dateComponents([.year, .month], from: date)
-        return calendar.date(from: components)!
+    func firstDayOfMonth(for date: Date) -> Date {
+        guard let firstDay = calendar.date(from: calendar.dateComponents([.year, .month], from: date)) else {
+            fatalError("Could not get first day of month")
+        }
+        return firstDay
     }
     
-    func weekDay(date: Date) -> Int
-    {
-        let components = calendar.dateComponents([.weekday], from: date)
-        return components.weekday!
+    func weekDay(for date: Date) -> Int {
+        guard let weekday = calendar.dateComponents([.weekday], from: date).weekday else {
+            fatalError("Could not get weekday")
+        }
+        return weekday
     }
     
-    func rangeOfPrayerStart(startDate: Date, firstDayOfMonth: Date) -> Int
-    {
-        let range = calendar.dateComponents([.day], from: startDate, to: firstDayOfMonth)
-        return range.day!
+    func rangeOfPrayerStart(from startDate: Date, to firstDayOfMonth: Date) -> Int {
+        guard let range = calendar.dateComponents([.day], from: startDate, to: firstDayOfMonth).day else {
+            fatalError("Could not get range of prayer start")
+        }
+        return range
     }
     
-    func addToDictionary(date: Date, index: String, name: String, dictionary: inout [Date: [String]])
-    {
+    func addToDictionary(date: Date, index: String, name: String, dictionary: inout [Date: [String]]) {
         dictionary[date] = [index, name]
     }
 }
