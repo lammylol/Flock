49b143c346e468f4d54dca94a0e904e835ab2bbe
lammylol <44993071+lammylol@users.noreply.github.com>
2024-09-04 14:16:15 -0700

    Prevent User From seeing Non-Accepted Profiles

--- Flock/Views/Main Views/Prayer Feed View/PostsFeed.swift
+++ Flock/Views/Main Views/Prayer Feed View/PostsFeed.swift
@@ -11,7 +11,7 @@ struct PostsFeed: View {
     
     @State var viewModel: FeedViewModel
     @Environment(UserProfileHolder.self) var userHolder
-    @State var person: Person
+    @Binding var person: Person
     @State var profileOrFeed: String = "feed"
     @State private var showSubmit: Bool = false
     
@@ -44,78 +44,92 @@ struct PostsFeed: View {
             if viewModel.prayerRequests.isEmpty {
                 if !viewModel.isFetching || !viewModel.isLoading {
                     do {
-                        person = try await userService.retrieveUserInfoFromUsername(person: person, userHolder: userHolder)
+                        person = try await userService.retrieveUserInfoFromUsername(person: person, userHolder: userHolder) // repetitive. Need to refactor later.
+                        if person.friendState == "sent" || person.friendState == "pending" {
+                            return
+                        }
+                        await viewModel.getPrayerRequests(user: userHolder.person, person: person)
+                        self.viewModel.prayerRequests = viewModel.prayerRequests
                     } catch {
-                        print(error)
+                           print(error)
                     }
-                    await viewModel.getPrayerRequests(user: userHolder.person, person: person)
-                    self.viewModel.prayerRequests = viewModel.prayerRequests
                 }
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
         .overlay {
-                if viewModel.prayerRequests.isEmpty && viewModel.isFinished {
-                    VStack{
-                        ContentUnavailableView {
-                            Label("No Posts Available...Yet!", systemImage: "list.bullet.rectangle.portrait")
-                        } description: {
-                            if profileOrFeed == "feed" {
-                                if viewModel.selectedStatus == .pinned {
-                                    Text("You will need to pin an exisiting post to see posts here")
-                                } else if viewModel.selectedStatus == .answered {
-                                    Text("No posts have been 'answered' yet. They will be featured here once you change the status to answered.")
-                                } else if viewModel.selectedStatus == .noLongerNeeded {
-                                    Text("No posts have been marked as 'no longer needed.'")
-                                } else {
-                                    Text("Link friends to your calendar to start seeing prayer requests. Or, you can add prayers to your profile or to a private account and they'll also be featured here.")
-                                }
+            if person.friendState == "pending" || person.friendState == "sent" { // overlay if profile has not been friended yet.
+                VStack{
+                    ContentUnavailableView {
+                        Label("Not Friends Yet", systemImage: "lock.square")
+                    } description: {
+                        Text("You must be friends with this account before you can see their posts.")
+                    }
+                    .frame(height: 200)
+                    .offset(y: 140)
+                    Spacer()
+                }
+            } else if viewModel.prayerRequests.isEmpty && viewModel.isFinished { // overlay for 'no posts available'
+                VStack{
+                    ContentUnavailableView {
+                        Label("No Posts Available...Yet!", systemImage: "list.bullet.rectangle.portrait")
+                    } description: {
+                        if profileOrFeed == "feed" {
+                            if viewModel.selectedStatus == .pinned {
+                                Text("You will need to pin an exisiting post to see posts here")
+                            } else if viewModel.selectedStatus == .answered {
+                                Text("No posts have been 'answered' yet. They will be featured here once you change the status to answered.")
+                            } else if viewModel.selectedStatus == .noLongerNeeded {
+                                Text("No posts have been marked as 'no longer needed.'")
                             } else {
-                                if viewModel.selectedStatus == .pinned {
-                                    Text("You will need to pin an exisiting post to see posts here")
-                                } else if viewModel.selectedStatus == .answered {
-                                    // Only show this if this account is saved under your userID.
-                                    if person.username == "" || person.userID == userHolder.person.userID {
-                                        Text("None of your posts have been 'answered' yet, but that's okay! They will be featured here once you change the status to answered.")
-                                    } else {
-                                        Text("\(person.firstName) has no answered posts yet.")
-                                    }
-                                } else if viewModel.selectedStatus == .answered {
-                                    Text("") // Empty. Just shows the main description.
+                                Text("Link friends to your calendar to start seeing prayer requests. Or, you can add prayers to your profile or to a private account and they'll also be featured here.")
+                            }
+                        } else {
+                            if viewModel.selectedStatus == .pinned {
+                                Text("You will need to pin an exisiting post to see posts here")
+                            } else if viewModel.selectedStatus == .answered {
+                                // Only show this if this account is saved under your userID.
+                                if person.username == "" || person.userID == userHolder.person.userID {
+                                    Text("None of your posts have been 'answered' yet, but that's okay! They will be featured here once you change the status to answered.")
                                 } else {
-                                    if person.username == "" || person.userID == userHolder.person.userID {
-                                        Text("Start adding posts to the list")
-                                    } else {
-                                        Text("\(person.firstName) has no posts to share.")
-                                    }
+                                    Text("\(person.firstName) has no answered posts yet.")
+                                }
+                            } else if viewModel.selectedStatus == .answered {
+                                Text("") // Empty. Just shows the main description.
+                            } else {
+                                if person.username == "" || person.userID == userHolder.person.userID {
+                                    Text("Start adding posts to the list")
+                                } else {
+                                    Text("\(person.firstName) has no posts to share.")
                                 }
                             }
-                        } actions: {
-                            if profileOrFeed == "feed" {
-                                if viewModel.selectedStatus == .current {
+                        }
+                    } actions: {
+                        if profileOrFeed == "feed" {
+                            if viewModel.selectedStatus == .current {
+                                Button(action: {showSubmit.toggle() })
+                                {
+                                    Text("Add a Post")
+                                        .font(.system(size: 16))
+                                }
+                            }
+                        } else {
+                            if viewModel.selectedStatus == .current {
+                                if person.username == "" || person.userID == userHolder.person.userID {
                                     Button(action: {showSubmit.toggle() })
                                     {
                                         Text("Add a Post")
                                             .font(.system(size: 16))
                                     }
                                 }
-                            } else {
-                                if viewModel.selectedStatus == .current {
-                                    if person.username == "" || person.userID == userHolder.person.userID {
-                                        Button(action: {showSubmit.toggle() })
-                                        {
-                                            Text("Add a Post")
-                                                .font(.system(size: 16))
-                                        }
-                                    }
-                                }
                             }
                         }
-                        .frame(height: 500)
-                        .offset(y: 140)
-                        Spacer()
                     }
-            }
+                    .frame(height: 500)
+                    .offset(y: 140)
+                    Spacer()
+                }
+        }
         }
         .sheet(isPresented: $showSubmit, onDismiss: {
             Task {
