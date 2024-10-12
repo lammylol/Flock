bd71ac4904507df35030d491bbe596ec20c3932c
lammylol <44993071+lammylol@users.noreply.github.com>
2024-10-12 08:49:19 -0700

    Updates to Post Card

--- Flock/Views/Model Views/Posts/PostRow.swift
+++ Flock/Views/Model Views/Posts/PostRow.swift
@@ -83,7 +83,7 @@ struct PostRow: View {
                             } else if post.postType == "Praise" {
                                 Text("Praise 🙌").font(.system(size: 12))
                             } else {
-                                Text("Post 📝").font(.system(size: 12))
+                                Text("Note 📝").font(.system(size: 12))
                             }
                             Spacer()
                         }
