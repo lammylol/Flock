be7d4d533cc486b0f8b688a33aed39d86e799cb2
pmar116 <preston.mar@live.com>
2025-03-18 15:24:05 -0700

    test

---
+++ FlockRN/components/Prayer/PrayerView/PrayerCard.tsx
@@ -0,0 +1,66 @@
+import { ThemedText } from '@/components/ThemedText';
+import { Prayer } from '@/types/firebase';
+import { router } from 'expo-router';
+import {
+  TouchableOpacity,
+  StyleSheet,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { Colors } from '@/constants/Colors';
+import Button from '@/components/Button';
+
+export interface PrayerCardProps {
+  prayer: Prayer;
+}
+
+export default function PrayerCard({ prayer }: PrayerCardProps): JSX.Element {
+  const colorScheme = useColorScheme() ?? 'light';
+
+  return (
+    <TouchableOpacity
+      style={[
+        styles.prayerContainer,
+        // { backgroundColor: Colors[colorScheme].background },
+        { backgroundColor: 'red' },
+      ]}
+      onPress={() => {
+        router.push({
+          pathname: '/prayers/prayerView',
+          params: { id: prayer.id },
+        });
+      }}
+    >
+      <ThemedText style={styles.title}>{prayer.title}</ThemedText>
+      <ThemedText numberOfLines={1} ellipsizeMode="tail" style={styles.content}>
+        {prayer.content}
+      </ThemedText>
+      <View style={styles.actionBar}>
+        <Button label={'Share'} onPress={() => {}} size="xs" />
+        <Button label={'Pray!'} onPress={() => {}} size="xs" />
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  // eslint-disable-next-line react-native/no-color-literals
+  prayerContainer: { backgroundColor: 'transparent', width: '100%' },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5, // Space between title and content
+  },
+  content: {
+    fontSize: 14,
+    color: 'gray', // Lighter color for the content
+    marginBottom: 5, // Space at the bottom
+  },
+
+  // eslint-disable-next-line react-native/no-color-literals
+  actionBar: {
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
