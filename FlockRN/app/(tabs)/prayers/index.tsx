b7c2f6eb8e08a1585260c7bc891d58fbf824ed6b
pmar116 <preston.mar@live.com>
2025-03-18 15:28:53 -0700

    update component name

---
+++ FlockRN/app/(tabs)/prayers/index.tsx
@@ -0,0 +1,87 @@
+import { StyleSheet, TextInput } from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { Prayer } from '@/types/firebase';
+import useAuth from '@/hooks/useAuth';
+import { useState } from 'react';
+import { prayerService } from '@/services/prayer/prayerService';
+
+import { useFocusEffect } from '@react-navigation/native';
+
+import PrayerCard from '@/components/Prayer/PrayerView/PrayerCard';
+import { Tabs } from '@/components/Tab';
+import { ThemedScrollView } from '@/components/ThemedScrollView';
+
+export default function TabTwoScreen() {
+  const { user } = useAuth();
+  const [userPrayers, setUserPrayers] = useState<Prayer[]>([]);
+  const [searchText, setSearchText] = useState('');
+  const [selectedTab, setSelectedTab] = useState<
+    'prayerPoints' | 'userPrayers'
+  >('userPrayers');
+
+  useFocusEffect(() => {
+    const fetchPrayers = async () => {
+      try {
+        if (!user) return;
+        const prayers = await prayerService.getUserPrayers(user.uid);
+        setUserPrayers(prayers);
+      } catch (error) {
+        console.error('Error fetching prayers:', error);
+      }
+    };
+    fetchPrayers();
+  });
+  return (
+    <ThemedScrollView>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">My Prayers</ThemedText>
+      </ThemedView>
+      <ThemedView>
+        <TextInput
+          style={styles.input}
+          placeholder="Search"
+          value={searchText}
+          onChangeText={setSearchText}
+        />
+        <Tabs
+          tabs={[`Prayer Requests (${userPrayers.length})`, `Prayers (${0})`]}
+          selectedIndex={selectedTab === 'prayerPoints' ? 0 : 1}
+          onChange={(index) =>
+            setSelectedTab(index === 0 ? 'prayerPoints' : 'userPrayers')
+          }
+          indicatorColor="#1976d2"
+          textColor="#000"
+        />
+        {selectedTab === 'prayerPoints' && (
+          <ThemedScrollView>
+            {userPrayers.map((prayer: Prayer) => (
+              <PrayerCard key={prayer.id} prayer={prayer} />
+            ))}
+          </ThemedScrollView>
+        )}
+        {selectedTab === 'userPrayers' && (
+          <ThemedScrollView>
+            {userPrayers.map((prayer: Prayer) => (
+              <PrayerCard key={prayer.id} prayer={prayer} />
+            ))}
+          </ThemedScrollView>
+        )}
+      </ThemedView>
+    </ThemedScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    borderRadius: 25,
+    borderWidth: 1,
+    height: 40,
+    marginBottom: 8,
+    paddingHorizontal: 8,
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
