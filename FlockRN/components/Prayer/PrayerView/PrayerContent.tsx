545600586645864df2e30c401e2dcbe2a1451b11
Matt Lam <matthewthelam@gmail.com>
2025-04-04 20:55:10 -0700

    Create Prayer Points UI

--- FlockRN/components/Prayer/PrayerView/PrayerContent.tsx
+++ FlockRN/components/Prayer/PrayerView/PrayerContent.tsx
@@ -1,28 +1,83 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
-import { Colors } from '@/constants/Colors';
 
 interface PrayerContentProps {
   title: string;
   content: string;
+  isEditMode?: boolean;
+  onTitleChange?: (newTitle: string) => void;
+  onContentChange?: (newContent: string) => void;
+  titlePlaceholder?: string;
+  contentPlaceholder?: string;
+  backgroundColor?: string;
 }
 
-const PrayerContent: React.FC<PrayerContentProps> = ({ title, content }) => {
+export function PrayerContent({
+  title,
+  content,
+  isEditMode,
+  onTitleChange,
+  onContentChange,
+  titlePlaceholder,
+  contentPlaceholder,
+  backgroundColor,
+}: PrayerContentProps): JSX.Element {
+  const [editableTitle, setEditableTitle] = useState(title);
+  const [editableContent, setEditableContent] = useState(content);
+
+  const handleTitleChange = (text: string) => {
+    setEditableTitle(text);
+    onTitleChange?.(text); // Pass updated title to parent if provided
+  };
+
+  const handleContentChange = (text: string) => {
+    setEditableContent(text);
+    onContentChange?.(text); // Pass updated content to parent if provided
+  };
+
   return (
-    <View style={styles.container}>
-      <ThemedText style={styles.titleText}>{title}</ThemedText>
-      <ThemedText style={styles.contentText}>{content}</ThemedText>
+    <View style={[styles.container, { backgroundColor: backgroundColor }]}>
+      {isEditMode ? (
+        <TextInput
+          style={[styles.titleText, styles.input]}
+          value={editableTitle}
+          placeholder={titlePlaceholder}
+          onChangeText={handleTitleChange}
+          multiline={true}
+          maxLength={100}
+        />
+      ) : (
+        <ThemedText style={styles.titleText}> {title}</ThemedText>
+      )}
+      {isEditMode ? (
+        <TextInput
+          style={[styles.contentText, styles.input]}
+          value={editableContent}
+          onChangeText={handleContentChange}
+          placeholder={contentPlaceholder}
+          multiline={true}
+        />
+      ) : (
+        <ThemedText style={styles.contentText}>{content}</ThemedText>
+      )}
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
+    borderRadius: 20,
+    flex: 0,
     gap: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
   },
   contentText: {
-    paddingVertical: 10,
+    fontSize: 16,
+  },
+  input: {
+    padding: 4,
   },
   titleText: {
     fontSize: 30,
