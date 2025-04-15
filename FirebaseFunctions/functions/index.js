d4add35b4393db6f61ec597d1807d32b64f0ee9d
Matt Lam <matthewthelam@gmail.com>
2025-04-15 06:39:32 -0700

    Added FirebaseFunctions

---
+++ FirebaseFunctions/functions/index.js
@@ -0,0 +1,14 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+import { getVectorEmbeddings } from "./firebaseCloudFunctions.js";
+
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
+
+export {getVectorEmbeddings};
