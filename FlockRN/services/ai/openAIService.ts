14d6d4018df369fcf7249bcf2bac1efb94239877
Matt Lam <matthewthelam@gmail.com>
2025-04-23 15:13:57 -0700

    Update openAIService.ts

--- FlockRN/services/ai/openAIService.ts
+++ FlockRN/services/ai/openAIService.ts
@@ -19,7 +19,7 @@ export default class OpenAiService {
   private static instance: OpenAiService; // Static instance to hold the singleton
 
   // Private constructor ensures that this class cannot be instantiated directly
-  private constructor() {}
+  private constructor() { }
 
   // Method to get the singleton instance
   public static getInstance(): OpenAiService {
@@ -58,8 +58,8 @@ export default class OpenAiService {
 
     const tagPrompt = `Tags: A list of up to 4 relevant tags for the prayer, selected from this list: ${allTags}`;
 
-    const prayerPointPrompt = `Prayer Points: Generate at most 3 prayer points. Each must be a type of either a 'prayer request' or 'praise'. 
-    Titles should be concise. Content should be clear and at most 50 words.`;
+    const prayerPointPrompt = `Prayer Points: Generate at most 3 prayer points. Each must be a type of either 'request', 'praise', or 'repentance'. 
+    Titles should include context and object with a max character limit of 10. Content should be clear and at most 50 words.`;
 
     const jsonFormat = `
     {
