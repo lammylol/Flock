206af8678e619cd4096708daa24368cb4579aea4
Matt Lam <matthewthelam@gmail.com>
2025-04-26 12:30:12 -0700

    Readd for now

--- FlockRN/types/firebase.ts
+++ FlockRN/types/firebase.ts
@@ -69,6 +69,7 @@ export interface PrayerPoint {
   tags?: PrayerType[];
   status?: Status;
   privacy?: Privacy;
+  prayerUpdates?: [];
   recipientName?: string;
   recipientId?: string;
   embedding?: number[];
